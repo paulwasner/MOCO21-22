6dab4203efc6f5adf6db5d8d9ec807f1d9862474
swienpah-th-koeln <sebastian.wienpahl@smail.th-koeln.de>
2022-01-22 11:45:42 +0100

    User-Klasse hinzugefügt

--- app/src/main/java/com/example/joinme/datastructure/Activity.kt
+++ app/src/main/java/com/example/joinme/datastructure/Activity.kt
@@ -1,5 +1,18 @@
 package com.example.joinme.datastructure
 
+import java.io.Serializable
+
 class Activity(val activityName: String, var started: Boolean)
 
 class Friends(val id: Int, val name: String)
+
+data class User(
+    val email: String? = null,
+    val password: String? = null,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val location: String? = null,
+    val activityState: String? = null,
+    val activityName: String? = null,
+    val friends: MutableList<String>?
+): Serializable
