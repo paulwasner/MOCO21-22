79b27fb923663c25ae63b045018f6d4923330f9c
swienpah-th-koeln <sebastian.wienpahl@smail.th-koeln.de>
2022-02-11 11:06:27 +0100

    Added SharedViewModel to exchange logged-in user between activities and fragments

---
+++ app/src/main/java/com/example/joinme/SharedViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.joinme
+
+import androidx.lifecycle.ViewModel
+import com.example.joinme.datastructure.User
+
+class SharedViewModel : ViewModel() {
+    var user = User("", "", "", "", "", "", "", mutableListOf())
+}
