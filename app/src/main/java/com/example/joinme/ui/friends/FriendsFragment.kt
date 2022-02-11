8c58cc9fbb4ca6199e57a6c4d63c1ab46e8eff76
swienpah-th-koeln <sebastian.wienpahl@smail.th-koeln.de>
2022-02-11 11:15:43 +0100

    Get logged-in user from SharedViewModel

--- app/src/main/java/com/example/joinme/ui/friends/FriendsFragment.kt
+++ app/src/main/java/com/example/joinme/ui/friends/FriendsFragment.kt
@@ -7,49 +7,49 @@ import android.view.ViewGroup
 import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.joinme.R
+import com.example.joinme.SharedViewModel
 import com.example.joinme.databinding.FragmentFriendsBinding
 import com.example.joinme.datastructure.Friends
+import com.example.joinme.datastructure.User
 import com.example.joinme.ui.activities.ActivityListAdapter
 
 class FriendsFragment : Fragment() {
 
-    private lateinit var friendsViewModel: FriendsViewModel
-    private var _binding: FragmentFriendsBinding? = null
+    private val sharedViewModel: SharedViewModel by activityViewModels()
+    private val friendsViewModel: FriendsViewModel by viewModels()
+    private lateinit var binding: FragmentFriendsBinding
+    private var user = User("","", "", "", "","", "", mutableListOf())
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        friendsViewModel =
-            ViewModelProvider(this).get(FriendsViewModel::class.java)
+        binding = FragmentFriendsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        _binding = FragmentFriendsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Angemeldeten User aus SharedViewModel holen
+        user = sharedViewModel.user
         val friends =
             arrayOf(
+                Friends(30, user.firstName!!),
                 Friends(4, "Bob"),
                 Friends(27, "Test")
             )
 
         val adapter = activity?.let { FriendsListAdapter(it, friends) }
-
-        val listView: ListView = root.findViewById(R.id.friends_listview)
+        val listView: ListView = binding.root.findViewById(R.id.friends_listview)
         listView.adapter = adapter
-
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
