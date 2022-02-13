eef78ff99796b062da3558342cee0a39cddf5f2e
swienpah-th-koeln <sebastian.wienpahl@smail.th-koeln.de>
2022-02-13 15:46:23 +0100

    Deleted unnecessary imports from FriendsFragment.kt

--- app/src/main/java/com/example/joinme/ui/friends/FriendsFragment.kt
+++ app/src/main/java/com/example/joinme/ui/friends/FriendsFragment.kt
@@ -5,18 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.joinme.R
 import com.example.joinme.SharedViewModel
 import com.example.joinme.databinding.FragmentFriendsBinding
 import com.example.joinme.datastructure.Friends
 import com.example.joinme.datastructure.User
-import com.example.joinme.ui.activities.ActivityListAdapter
+
 
 class FriendsFragment : Fragment() {
 
