0c1b0540201961b85cf431ade53fcc422439026b
swienpah-th-koeln <sebastian.wienpahl@smail.th-koeln.de>
2022-02-13 15:48:18 +0100

    Added addButton logic to add a friend to users friendlist

--- app/src/main/java/com/example/joinme/ui/friends/FriendDetailFragment.kt
+++ app/src/main/java/com/example/joinme/ui/friends/FriendDetailFragment.kt
@@ -1,16 +1,28 @@
 package com.example.joinme.ui.friends
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.joinme.SharedViewModel
 import com.example.joinme.databinding.FragmentFriendDetailBinding
-import com.example.joinme.databinding.FragmentFriendsBinding
+import com.example.joinme.datastructure.User
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class FriendDetailFragment : Fragment() {
+    //Firebase
+    val database = FirebaseDatabase.getInstance(
+        "https://joinme-f75c5-default-rtdb.europe-west1.firebasedatabase.app/")
+    val userRef = database.getReference("users")
+    val emailRef = database.getReference("emails")
+
     private val sharedViewModel: SharedViewModel by activityViewModels()
     private var _binding: FragmentFriendDetailBinding? = null
     private val binding get() = _binding!!
@@ -33,11 +45,85 @@ class FriendDetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.addButton.setOnClickListener {
-            //TODO Aktuellen User holen (SharedViewModel)
-            //TODO Prüfen, ob eingegebener Freund in DB existiert
-                //TODO Wenn ja, Freund zur Freundesliste hinzufügen (User aktuallisieren: SharedViewModel)
-                    //TODO List of Friends in FriendsFragment aktuallisieren
-                //TODO Wenn nein, Toast / Dialog + zum letzten Fragment zurückkenren
+            //Aktuellen User + UUID holen
+            val user = sharedViewModel.user
+            val uuid = sharedViewModel.uuid
+
+            val newFriend = binding.friendDetailEmail.text.toString()
+            var friendId: String
+
+            //Freundesliste holen
+            val listOfFriends = user.friends
+            val newFriendsList: MutableList<String> = mutableListOf()
+
+            //Existenz-Flag
+            var existenceFlag = 0
+
+            //Freunde der alten Liste in neue Liste schreiben
+            listOfFriends?.forEach {
+                    newFriendsList.add(it)
+            }
+            Log.d("LIST1", listOfFriends.toString())
+            Log.d("LIST2", newFriendsList.toString())
+
+            //Prüfen, ob eingegebener Freund in DB existiert
+            when {
+                newFriend.isEmpty() -> {
+                    Toast.makeText(activity, "Bitte Feld ausfüllen!", Toast.LENGTH_SHORT).show()
+                }
+                newFriend == user.email -> {
+                    Toast.makeText(activity, "Benutzer kann nicht hinzugefügt werden!", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    //Prüfen, ob Freund in DB exitiert
+                    emailRef.addListenerForSingleValueEvent(object : ValueEventListener{
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            if( snapshot.hasChild(newFriend) ) {
+                                friendId = snapshot.child(newFriend).value as String
+
+                                //Prüfen, ob Freund bereits in der Liste
+                                if( !newFriendsList.contains(friendId) ) {
+                                    //Neuen Freund hinzufügen
+                                    newFriendsList.add(friendId)
+                                }
+                                else {
+                                    existenceFlag = 1
+                                }
+
+                                //Wenn Freund bereits in der Liste
+                                if( existenceFlag == 1 ) {
+                                    Toast.makeText(activity, "Benutzer bereits hinzugefügt", Toast.LENGTH_SHORT).show()
+                                }
+                                else {
+                                    //User aktuallisieren
+                                    val updatedUser = User(
+                                        user.email, user.password, user.firstName, user.lastName, user.location,
+                                        user.activityState, user.activityName, newFriendsList
+                                    )
+                                    //User mit neuem Freund in DB speichen
+                                    userRef.child(uuid).setValue(updatedUser)
+                                    //User in SharedViewModel updaten
+                                    sharedViewModel.user = updatedUser
+                                    Toast.makeText(activity, "Freund \"$newFriend\" hinzugefügt", Toast.LENGTH_SHORT).show()
+                                }
+                                //Aus Fragment heraus wechesln
+                                activity?.onBackPressed()
+
+                                //TODO FreundesListe in FriendsFragment updaten
+                                //TODO In onStart des FriendFragments jeweils Freundesliste updaten + anzeigen
+                            } else {
+                                //Freund existiert nicht
+                                Toast.makeText(activity, "Freund exitiert nicht", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                            TODO("Not yet implemented")
+                        }
+
+                    })
+                }
+            }
         }
     }
 }
