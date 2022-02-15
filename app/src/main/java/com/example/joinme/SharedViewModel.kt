e9247b1be340ada08fb3a0cbd0b558d8ba4dd08f
swienpah-th-koeln <sebastian.wienpahl@smail.th-koeln.de>
2022-02-15 16:56:49 +0100

    Created new property listOfFriends to hold a list of Friends (id, name)

--- app/src/main/java/com/example/joinme/SharedViewModel.kt
+++ app/src/main/java/com/example/joinme/SharedViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.joinme
 
 import androidx.lifecycle.ViewModel
+import com.example.joinme.datastructure.Friends
 import com.example.joinme.datastructure.User
 
 class SharedViewModel : ViewModel() {
     var user = User("", "", "", "", "", "", "", mutableListOf())
     var uuid = ""
+    var listOfFriends: MutableList<Friends> = mutableListOf()
 }
