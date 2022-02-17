2147893f28ebfce4f5dabc36c121cb659ea54de8
swienpah-th-koeln <sebastian.wienpahl@smail.th-koeln.de>
2022-02-17 15:14:04 +0100

    Added functions checkLocationPermission() + requestLocationPermission() in ActivitiesFragment.kt for requesting location permissions

--- app/src/main/java/com/example/joinme/ui/activities/ActivitiesFragment.kt
+++ app/src/main/java/com/example/joinme/ui/activities/ActivitiesFragment.kt
@@ -1,16 +1,22 @@
 package com.example.joinme.ui.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.ListView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import com.example.joinme.R
+import com.example.joinme.SharedViewModel
 import com.example.joinme.databinding.FragmentActivitiesBinding
 import com.example.joinme.datastructure.Activity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
 class ActivitiesFragment : Fragment() {
@@ -48,11 +54,42 @@ class ActivitiesFragment : Fragment() {
         val listView: ListView = root.findViewById(R.id.activity_listview)
         listView.adapter = adapter
 
+        if(!checkLocationPermission()) {
+            requestLocationPermission()
+        }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        return PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(
+            requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+    }
+
+    private fun requestLocationPermission() {
+        val requestPermissionLauncher = registerForActivityResult( ActivityResultContracts.RequestPermission() )
+        { isGranted: Boolean ->
+            if (isGranted) {
+                MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("Standort-Zugriff")
+                    .setMessage("Der Standort-Zugriff wurde erteilt.")
+                    .setCancelable(true)
+                    .show()
+            } else {
+                MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("Standort-Zugriff")
+                    .setMessage("Der Standort-Zugriff wurde verweigert.\r\n" +
+                            "Um eine Aktivität starten zu können, muss der " +
+                            "Standort-Zugriff erlaubt werden.")
+                    .setCancelable(true)
+                    .show()
+            }
+        }
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
     }
 }
