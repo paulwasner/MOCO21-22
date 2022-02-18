48d3bb1e93f61c13f9b63f7c86862b562f2fa572
swienpah-th-koeln <sebastian.wienpahl@smail.th-koeln.de>
2022-02-18 17:33:15 +0100

    Added FusedLocationClient to access last known location

--- app/src/main/java/com/example/joinme/ui/activities/ActivityListAdapter.kt
+++ app/src/main/java/com/example/joinme/ui/activities/ActivityListAdapter.kt
@@ -4,11 +4,14 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.pm.PackageManager
+import android.location.Location
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.core.content.ContextCompat
 import com.example.joinme.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.example.joinme.datastructure.Activity as ActivityData
 
 
@@ -20,26 +23,39 @@ class ActivityListAdapter(
 
     @SuppressLint("SetTextI18n")
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
-
         val inflater = context.layoutInflater
         val rowView = inflater.inflate(R.layout.activities_listtile, null, true)
 
         val activityName = rowView.findViewById<TextView>(R.id.listtile_title)
         val startActivityButton = rowView.findViewById<TextView>(R.id.listtile_button)
-        startActivityButton.setOnClickListener {
 
+        var lastLocation: Location? = null
+        val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                // Got last known location. In some rare situations this can be null.
+                lastLocation = location
+            }
+
+        startActivityButton.setOnClickListener {
             val permissionGranted = PackageManager.PERMISSION_GRANTED == ContextCompat
                         .checkSelfPermission( context, Manifest.permission.ACCESS_COARSE_LOCATION)
 
 
             if(!activities[position].started && !checkActivityStarted()){
                 //Aktivität starten, wenn Permission gegeben ist
-                    if( permissionGranted ) {
-                        startActivityButton.text = "Teilen beenden"
-                        activities[position].started = true
-                    }
+                if( permissionGranted && lastLocation != null ) {
+                    startActivityButton.text = "Teilen beenden"
+                    activities[position].started = true
+
+                    Toast.makeText(context, "Standort: ${lastLocation!!.latitude}, ${lastLocation!!.longitude}", Toast.LENGTH_SHORT).show()
+                }
+                else if( permissionGranted ){
+                    //Permisson granted aber kein Zugriff auf letzten Standort
+                    Toast.makeText(context, "Der letzte bekannte Standort kann nicht abgerufen werden!", Toast.LENGTH_SHORT).show()
+                }
                 else {
-                    Toast.makeText(context, "Standort freigabe nicht erteilt!", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, "Standort-Freigabe nicht erteilt!", Toast.LENGTH_SHORT).show()
                     }
             }
             else if(activities[position].started) {
