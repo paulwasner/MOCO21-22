64de0b5225c6001217b1c68a47a84100a403f9ac
swienpah-th-koeln <sebastian.wienpahl@smail.th-koeln.de>
2022-02-21 19:21:47 +0100

    Fixed typo in function name uptateTopSttaus() in MainActivity.kt

--- app/src/main/java/com/example/joinme/MainActivity.kt
+++ app/src/main/java/com/example/joinme/MainActivity.kt
@@ -50,7 +50,7 @@ class MainActivity : AppCompatActivity() {
         sharedViewModel.uuid = uuid
 
         //ActivityList updaten
-        updateTopStaus()
+        updateTopStatus()
         //Freundesliste laden
         val friendsId = sharedViewModel.user.friends
 
@@ -72,7 +72,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun updateTopStaus() {
+    private fun updateTopStatus() {
         if (sharedViewModel.user.activityState == true.toString()) {
             sharedViewModel.activityArray.forEach {
                 if (it.activityName == sharedViewModel.user.activityName) {
