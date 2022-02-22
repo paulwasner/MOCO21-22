ad6fbe03c6509173da03bcf652eb02e4392b2d94
swienpah-th-koeln <sebastian.wienpahl@smail.th-koeln.de>
2022-02-22 10:58:59 +0100

    Moved registrationButton clickListener-Logic from RegistrationActivity.kt to RegistrationViewModel.kt

--- app/src/main/java/com/example/joinme/ui/registration/RegistrationActivity.kt
+++ app/src/main/java/com/example/joinme/ui/registration/RegistrationActivity.kt
@@ -3,6 +3,7 @@ package com.example.joinme.ui.registration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
 import com.example.joinme.R
 import com.example.joinme.databinding.ActivityRegistrationBinding
 import com.example.joinme.datastructure.User
@@ -16,13 +17,14 @@ import java.util.*
 class RegistrationActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityRegistrationBinding
+    val registrationViewModel: RegistrationViewModel by viewModels()
 
     //Firebase
     private val database = FirebaseDatabase.getInstance(
         "https://joinme-f75c5-default-rtdb.europe-west1.firebasedatabase.app/"
     )
-    private val userRef = database.getReference("users")
-    private val emailRef = database.getReference("emails")
+    val userRef = database.getReference("users")
+    val emailRef = database.getReference("emails")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,72 +33,14 @@ class RegistrationActivity : AppCompatActivity() {
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val firstname = binding.firstname
-        val lastname = binding.lastname
-        val email = binding.email
-        val password = binding.password
-        val passwordConf = binding.confirmPassword
-
         val registrationButton = binding.registrationButton
         val loginNowButton = binding.loginNowButton
 
 
         registrationButton.setOnClickListener {
-            //Benutzerdaten einlesen
-            val firstnameTxt = firstname.text.toString()
-            val lastnameTxt = lastname.text.toString()
-            val emailTxt = email.text.toString()
-            val passwordTxt = password.text.toString()
-            val passwordConfTxt = passwordConf.text.toString()
-
-            //Prüfen, ob alle Felder ausgefüllt wurden
-            if (firstnameTxt.isEmpty() || lastnameTxt.isEmpty() || emailTxt.isEmpty() ||
-                passwordTxt.isEmpty() || passwordConfTxt.isEmpty()
-            ) {
-
-                //Benutzer auf unausgefüllte Felder hinweisen
-                Toast.makeText(applicationContext, "Bitte alle Felder ausfüllen",
-                    Toast.LENGTH_SHORT).show()
-            }
-
-            //Passwörter überprüfen
-            else if (passwordTxt != passwordConfTxt) {
-                Toast.makeText(applicationContext, "Passwörter stimmen nicht überein!",
-                    Toast.LENGTH_SHORT).show()
-            }
-
-            //Wenn keine Fehler in der Eingabe vorleigen
-            else {
-                //Neues User-Objekt erstellen
-                val user = User(emailTxt, passwordTxt, firstnameTxt, lastnameTxt, "",
-                    "false", "", mutableListOf())
-
-                //Prüfen, ob Email registriert
-                emailRef.addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        if (snapshot.hasChild(user.email!!)) {
-                            Toast.makeText(applicationContext, "Email ist bereits registriert!",
-                                Toast.LENGTH_SHORT).show()
-                        } else if (!snapshot.hasChild(user.email)) {
-                            //UUID für neuen Nutzer generieren
-                            val uuid = UUID.randomUUID().toString()
-                            //User in DB einfügen
-                            userRef.child(uuid).setValue(user)
-                            //Zuweisung Email zu UUID für besseres Querrying
-                            emailRef.child(emailTxt).setValue(uuid)
-                            //Benutzer über Registrierung informieren
-                            Toast.makeText(applicationContext, "User erfolgreich registriert",
-                                Toast.LENGTH_SHORT).show()
-                            finish()
-                        }
-                    }
-
-                    override fun onCancelled(error: DatabaseError) {
-                        //TODO
-                    }
-                })
-            }
+            registrationViewModel.registrationClickListener(binding, this)
         }
+
         //Falls Account bereits existiert, zum Login weiterleiten
         loginNowButton.setOnClickListener {
             finish()
---
+++ app/src/main/java/com/example/joinme/ui/registration/RegistrationViewModel.kt
@@ -0,0 +1,73 @@
+package com.example.joinme.ui.registration
+
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import com.example.joinme.databinding.ActivityRegistrationBinding
+import com.example.joinme.datastructure.User
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import java.util.*
+
+class RegistrationViewModel : ViewModel() {
+    fun registrationClickListener(
+        binding: ActivityRegistrationBinding,
+        activity: RegistrationActivity
+    ) {
+        val context = activity.applicationContext
+
+        //Benutzerdaten einlesen
+        val firstnameTxt = binding.firstname.text.toString()
+        val lastnameTxt = binding.lastname.text.toString()
+        val emailTxt = binding.email.text.toString()
+        val passwordTxt = binding.password.text.toString()
+        val passwordConfTxt = binding.confirmPassword.text.toString()
+
+        //Prüfen, ob alle Felder ausgefüllt wurden
+        if (firstnameTxt.isEmpty() || lastnameTxt.isEmpty() || emailTxt.isEmpty() ||
+            passwordTxt.isEmpty() || passwordConfTxt.isEmpty())
+        {
+            //Benutzer auf unausgefüllte Felder hinweisen
+            Toast.makeText(context, "Bitte alle Felder ausfüllen",Toast.LENGTH_SHORT).show()
+        }
+
+        //Passwörter überprüfen
+        else if (passwordTxt != passwordConfTxt) {
+            Toast.makeText(context, "Passwörter stimmen nicht überein!",Toast.LENGTH_SHORT).show()
+        }
+
+        //Wenn keine Fehler in der Eingabe vorleigen
+        else {
+            //Neues User-Objekt erstellen
+            val user = User(
+                emailTxt, passwordTxt, firstnameTxt, lastnameTxt, "",
+                "false", "", mutableListOf()
+            )
+
+            //Prüfen, ob Email registriert
+            activity.emailRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.hasChild(user.email!!)) {
+                        Toast.makeText(context, "Email ist bereits registriert!",
+                            Toast.LENGTH_SHORT).show()
+                    } else if (!snapshot.hasChild(user.email)) {
+                        //UUID für neuen Nutzer generieren
+                        val uuid = UUID.randomUUID().toString()
+                        //User in DB einfügen
+                        activity.userRef.child(uuid).setValue(user)
+                        //Zuweisung Email zu UUID für besseres Querrying
+                        activity.emailRef.child(emailTxt).setValue(uuid)
+                        //Benutzer über Registrierung informieren
+                        Toast.makeText(context, "User erfolgreich registriert",
+                            Toast.LENGTH_SHORT).show()
+                        activity.finish()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    //TODO
+                }
+            })
+        }
+    }
+}
